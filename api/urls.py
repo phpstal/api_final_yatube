f83f979220c9acb51becd91b10cf6156941ec20f
Vladimir <vrmy87@yandex.ru>
2021-03-05 20:22:57 +0700

    made the project of eleventh sprint

--- api/urls.py
+++ api/urls.py
@@ -10,6 +10,11 @@ router_v1 = DefaultRouter()
 router_v1.register('posts', PostViewSet)
 router_v1.register('group', GroupViewSet)
 router_v1.register('follow', FollowViewSet, basename='follow')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 API_V = 'v1/'
 
 urlpatterns = [
